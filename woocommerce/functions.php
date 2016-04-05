5aebf3016cb3585a6999d294c45cd4aa20188b8a
adiraomj <adiraomj@outlook.com>
2016-04-05 10:55:29 +0530

    Styled and added the checkout template

--- woocommerce/functions.php
+++ woocommerce/functions.php
@@ -1,17 +1,10 @@
 <?php
 
 function siteorigin_unwind_woocommerce_change_hooks(){
-	// Move the price higher
-	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
-	add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 4 );
 
-	// Move the
-	remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
-	add_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 35);
+	// Load template related functions.
+	include get_template_directory() . '/woocommerce/template-tags.php';
 
-	// Use a custom upsell function to change number of items
-	remove_action('woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
-	add_action('woocommerce_after_single_product_summary', 'siteorigin_unwind_woocommerce_output_upsells', 15);
 }
 add_action('after_setup_theme', 'siteorigin_unwind_woocommerce_change_hooks');
 
