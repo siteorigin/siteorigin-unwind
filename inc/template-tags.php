bcd185e820a2f6bd8d4d6ebedfe221edff6bedb6
Andrew Misplon <amisplon@gmail.com>
2016-09-29 13:55:09 +0200

    Updated no comments string
    Changed no comments link to Post a Comment.
    Removed default `posted_on` _s function.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -88,38 +88,6 @@ function siteorigin_unwind_posts_navigation() {
 }
 endif;
 
-if ( ! function_exists( 'siteorigin_unwind_posted_on' ) ) :
-/**
- * Prints HTML with meta information for the current post-date/time and author.
- */
-function siteorigin_unwind_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
-
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
-
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'siteorigin-unwind' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
-
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'siteorigin-unwind' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline">' . $byline . '</span>'; // WPCS: XSS OK.
-
-}
-endif;
-
 if ( ! function_exists( 'siteorigin_unwind_post_meta' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time, category and comment count.
@@ -133,11 +101,11 @@ function siteorigin_unwind_post_meta() {
 
 	if ( comments_open() ) {
 		if ( $num_comments == 0 ) {
-			$comments = esc_html__('Comments');
+			$comments = esc_html__( 'Post a Comment' );
 		} elseif ( $num_comments > 1 ) {
-			$comments = $num_comments . esc_html__(' Comments');
+			$comments = $num_comments . esc_html__( ' Comments' );
 		} else {
-			$comments = esc_html__('1 Comment');
+			$comments = esc_html__( '1 Comment' );
 		}
 	} else {
 		$comments = NULL;
