5920046c7153c853508975c32db32a99410b3ee8
Andrew Misplon <amisplon@gmail.com>
2016-09-29 17:50:29 +0200

    PHP info update

--- single.php
+++ single.php
@@ -5,6 +5,8 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package siteorigin-unwind
+ * @since siteorigin-unwind 0.1
+ * @license GPL 2.0
  */
 
 get_header(); ?>
