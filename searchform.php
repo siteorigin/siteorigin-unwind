40a451dab2f17f9fd59191faeed9df75844d91f0
Andrew Misplon <amisplon@gmail.com>
2016-10-02 15:47:52 +0200

    Search widget styling

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" action="<?php echo esc_url( site_url() ) ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( site_url() ) ?>">
 	<input type="search" name="s" placeholder="<?php esc_attr_e( 'Search', 'siteorigin-unwind') ?>" value="<?php echo get_search_query() ?>" />
-	<input type="submit" value="<?php esc_attr_e('Search', 'siteorigin-unwind') ?>" />
+	<input type="submit" class="search-submit" value="<?php esc_attr_e('Search', 'siteorigin-unwind') ?>" />
 </form>
