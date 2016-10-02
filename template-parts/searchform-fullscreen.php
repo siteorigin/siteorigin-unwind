a53aa2a81942edb776020a9e634ceafeb3180853
Andrew Misplon <amisplon@gmail.com>
2016-10-02 19:01:28 +0200

    Full screen search adjustments

--- template-parts/searchform-fullscreen.php
+++ template-parts/searchform-fullscreen.php
@@ -14,5 +14,8 @@
 <form method="get" action="<?php echo esc_url( site_url() ) ?>">
 	<h3><?php esc_html_e( 'Search Site', 'siteorigin-unwind' ); ?></h3>
 	<input type="search" name="s" placeholder="<?php esc_attr_e( 'Type and hit enter to search', 'siteorigin-unwind') ?>" value="<?php echo get_search_query() ?>" />
-	<input type="submit" value="<?php esc_attr_e( 'Search', 'siteorigin-unwind' ); ?>" />
+	<button type="submit">
+		<label class="screen-reader-text"><?php esc_html_e( 'Search', 'siteorigin-unwind' ); ?></label>
+		<?php siteorigin_unwind_display_icon( 'search' ); ?>
+	</button>	
 </form>
