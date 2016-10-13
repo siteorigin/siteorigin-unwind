aecc5a1ba1e34a8c71abfba0f8343fd8affbce6a
Andrew Misplon <amisplon@gmail.com>
2016-10-13 22:16:35 +0200

    Switch to standard handle for FlexSlider
    Standard handles about to be adopted as required.
    Added JS prefix constant.

--- functions.php
+++ functions.php
@@ -174,12 +174,12 @@ function siteorigin_unwind_scripts() {
 
 	// Flexslider.
 	if ( is_home() && siteorigin_setting( 'blog_featured_slider' ) && siteorigin_unwind_has_featured_posts( 5 ) ) {
-		wp_enqueue_style( 'siteorigin-flexslider', get_template_directory_uri() . '/css/flexslider.css', array() );
-		wp_enqueue_script( 'siteorigin-flexslider-script', get_template_directory_uri() . '/js/jquery.flexslider.js', array( 'jquery' ) );
+		wp_enqueue_style( 'siteorigin-unwind-flexslider', get_template_directory_uri() . '/css/flexslider.css' );
+		wp_enqueue_script( 'jquery-flexslider', get_template_directory_uri() . '/js/jquery.flexslider' . SITEORIGIN_THEME_JS_PREFIX . '.js', array( 'jquery' ), '2.6.3', true );
 	}
 
 	// Theme JavaScript.
-	wp_enqueue_script( 'siteorigin-unwind-script', get_template_directory_uri() . '/js/unwind.js', array('jquery') );
+	wp_enqueue_script( 'siteorigin-unwind-script', get_template_directory_uri() . '/js/unwind' . SITEORIGIN_THEME_JS_PREFIX . '.js', array('jquery') );
 
 	// Skip link focus fix.
 	wp_enqueue_script( 'siteorigin-unwind-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
