83f8b09dda304ea4b169db05b805e6d2c783e19a
Andrew Misplon <amisplon@gmail.com>
2016-10-31 17:42:01 +0200

    WooCommerce shop sidebar

---
+++ sidebar-shop.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The sidebar for WooCommerce shop pages.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package siteorigin-unwind
+ * @since siteorigin-unwind 1.0
+ * @license GPL 2.0
+ */
+
+if ( ! is_active_sidebar( 'shop-sidebar' ) ) return;
+if ( ! in_array( siteorigin_page_setting( 'layout', 'default' ), array( 'default','full-width-sidebar' ), true )  ) return;
+?>
+
+<aside id="secondary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'shop-sidebar' ); ?>
+</aside><!-- #secondary -->
