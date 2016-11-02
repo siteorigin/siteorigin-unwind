f282d382f553a41046966a3efee451bad8758603
Andrew Misplon <amisplon@gmail.com>
2016-11-02 21:48:21 +0200

    Style the comments page navigation

--- comments.php
+++ comments.php
@@ -41,8 +41,8 @@ if ( post_password_required() ) {
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'siteorigin-unwind' ); ?></h2>
 			<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'siteorigin-unwind' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'siteorigin-unwind' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( '<span>&larr;</span>' . esc_html__( 'Older comments', 'siteorigin-unwind' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer comments', 'siteorigin-unwind' ) . '<span>&rarr;</span>' ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
@@ -64,8 +64,8 @@ if ( post_password_required() ) {
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'siteorigin-unwind' ); ?></h2>
 			<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'siteorigin-unwind' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'siteorigin-unwind' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( '<span>&larr;</span>' . esc_html__( 'Older comments', 'siteorigin-unwind' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer comments', 'siteorigin-unwind' ) . '<span>&rarr;</span>' ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
