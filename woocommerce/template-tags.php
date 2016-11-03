29838f87b095525289f3d3483aed658bbd868f5d
adiraomj <adiraomj@outlook.com>
2016-11-03 19:42:50 +0530

    Basic layout of product archive item

--- woocommerce/template-tags.php
+++ woocommerce/template-tags.php
@@ -27,3 +27,13 @@ function siteorigin_unwind_woocommerce_description_title() {
 	return '';
 }
 add_filter( 'woocommerce_product_description_heading', 'siteorigin_unwind_woocommerce_description_title' );
+
+// Modify Archive Content
+//remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+//remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+//remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+//remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
+
+//remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
+remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
