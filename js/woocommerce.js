28ba5e6083cfcd3d49cb2d573f0d21c92f572e47
adiraomj <adiraomj@outlook.com>
2016-11-04 23:09:59 +0530

    Styled product archive ordering select

--- js/woocommerce.js
+++ js/woocommerce.js
@@ -70,4 +70,43 @@ jQuery( function($){
 		} );
 	} );
 
+	// Convert the dropdown
+	$('.woocommerce-ordering select').each( function(){
+		var $$ = $(this);
+
+		var c = $('<div></div>')
+			.html( '<span class="current">' + $$.find(':selected').html() + '</span>' + so_unwind_data.chevron_down )
+			.addClass('ordering-selector-wrapper')
+			.insertAfter( $$ );
+
+		var dropdownContainer = $('<div/>')
+			.addClass('ordering-dropdown-container')
+			.appendTo(c);
+
+		var dropdown = $('<ul></ul>')
+			.addClass('ordering-dropdown')
+			.appendTo(dropdownContainer);
+
+		var widest = 0;
+		$$.find( 'option' ).each( function(){
+			var $o = $(this);
+			dropdown.append(
+				$("<li></li>")
+					.html( $o.html() )
+					.data( 'val', $o.attr('value') )
+					.click( function(){
+						$$.val( $(this).data('val') );
+						$$.closest('form').submit();
+					} )
+			);
+
+			widest = Math.max( c.find('.current').html( $o.html() ).width(), widest);
+
+		} );
+
+		c.find('.current').html( $$.find(':selected').html()).width( widest );
+
+		$$.hide();
+	} );
+
 } );
