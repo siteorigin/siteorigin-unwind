db5810e0abd75088968daae0a33647365071270d
Andrew Misplon <amisplon@gmail.com>
2016-11-14 20:31:22 +0200

    Don’t show related posts on attachment page

--- single.php
+++ single.php
@@ -34,7 +34,7 @@ get_header(); ?>
 				siteorigin_unwind_author_box();
 			endif;
 
-			if ( siteorigin_setting( 'blog_display_related_posts' ) ) :
+			if ( ! is_attachment() && siteorigin_setting( 'blog_related_posts' ) ) :
 				siteorigin_unwind_related_posts( $post->ID );
 			endif;
 
