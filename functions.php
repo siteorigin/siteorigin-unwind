86165f52c7fd04145b8c3217604848be77cca1a0
gregpriday <greg@siteorigin.com>
2016-11-23 20:08:19 +0200

    Readded comment

--- functions.php
+++ functions.php
@@ -135,6 +135,9 @@ function siteorigin_unwind_setup() {
 	 */
 	add_theme_support( 'siteorigin-template-settings' );
 
+	/**
+	 * Add the theme's custom WooCommerce functions.
+	 */
 	if ( function_exists( 'is_woocommerce' ) && is_woocommerce() ) {
 		require get_template_directory() . '/woocommerce/functions.php';
 	}
