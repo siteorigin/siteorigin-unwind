427030242b11799a89d478c2aeb71a5daef2ac40
adiraomj <adiraomj@outlook.com>
2017-07-14 13:11:40 +0530

    blog left info initial commit

---
+++ template-parts/content-left-info.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Template part for displaying left info posts
+ *
+ * @package siteorigin-unwind
+ * @license GPL 2.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'archive-entry' ); ?>>
+
+	<div class="entry-thumbnail">
+		<?php if( has_post_thumbnail() && siteorigin_setting( 'blog_featured_archive' ) ) : ?>
+			<a href="<?php the_permalink() ?>">
+				<?php the_post_thumbnail( 'post-thumbnail', array( 'class' => 'aligncenter' ) ); ?>
+			</a>
+		<?php endif; ?>
+
+		<div class="entry-cats">
+			<?php the_category( ' ' ); ?>
+		</div>
+	</div>
+
+
+	<header class="entry-header">
+		<?php if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php siteorigin_unwind_post_meta( false ); ?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_excerpt();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links"><span class="page-links-title">' . esc_html__( 'Pages:', 'siteorigin-unwind' ) . '</span>',
+				'after'  => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+			) );
+		?>
+		<p><a href="<?php the_permalink() ?>"><?php echo esc_html__( 'Continue Reading', 'siteorigin-unwind' ) ?> &rarr;</a></p>
+	</div><!-- .entry-content -->
+
+</article>
