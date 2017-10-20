cacb699565c97a74a9092995657452fbaf89c87e
Andrew Misplon <amisplon@gmail.com>
2017-10-20 15:34:51 +0200

    Remove additional landmark role

--- page.php
+++ page.php
@@ -19,7 +19,7 @@ get_header(); ?>
 	<?php siteorigin_unwind_breadcrumbs(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main">
 
 			<?php
 			while ( have_posts() ) : the_post();
