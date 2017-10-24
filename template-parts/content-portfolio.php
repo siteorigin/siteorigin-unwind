18bca1896ebc38cebfd982b93f7855055c1271d3
Andrew Misplon <amisplon@gmail.com>
2017-10-24 12:34:21 +0200

    Make portfolio thumb clickable

--- template-parts/content-portfolio.php
+++ template-parts/content-portfolio.php
@@ -7,7 +7,7 @@
  * @license GPL 2.0
  */
 
-// get Jetpack Portfolio taxonomy terms for portfolio filtering
+// Get Jetpack Portfolio taxonomy terms for portfolio filtering.
 $terms = get_the_terms( $post->ID, 'jetpack-portfolio-type' );
 
 if ( $terms && ! is_wp_error( $terms ) ) :
@@ -29,13 +29,15 @@ endif; ?>
 <article id="post-<?php the_ID(); ?> <?php echo $types; ?>" <?php post_class( $classes ); ?>>
 
 	<div class="entry-thumbnail">
-		<?php the_post_thumbnail( 'siteorigin-unwind-500x500-crop' ); ?>
+		<a href="<?php the_permalink(); ?>">
+			<div class="entry-overlay"></div>
+			<div class="entry-content">
+				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+				<div class="entry-divider"></div>
+				<p class="entry-project-type"><?php echo $filtering; ?></p>					
+			</div>		
+			<?php the_post_thumbnail( 'siteorigin-unwind-500x500-crop' ); ?>
+		</a>
 	</div><!-- .entry-thumbnail -->
-
-	<div class="entry-overlay">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-		<div class="entry-divider"></div>
-		<p class="entry-project-type"><?php echo $filtering; ?></p>
-	</div><!-- .entry-overlay -->
 
 </article><!-- #post-## -->
