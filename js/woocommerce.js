24ac25b04218429a002abbbbf5fad9cde01e1702
Andrew Misplon <amisplon@gmail.com>
2018-08-08 09:49:04 +0200

    WooCommerce archive button hover improvements

--- js/woocommerce.js
+++ js/woocommerce.js
@@ -109,6 +109,71 @@ jQuery( function( $ ) {
 		$( '.product-images-carousel' ).find( '.product-featured-image' ).click();
 	} );
 
+	// Quick View Modal.
+	$( '.product-quick-view-button' ).click( function( e ) {
+		e.preventDefault();
+
+		var $container = '#quick-view-container';
+		var $content = '#product-quick-view';
+
+		var id = $( this ).attr( 'data-product-id' );
+
+		$.post(
+			so_unwind_data.ajaxurl,
+			{ action: 'so_product_quick_view', product_id: id },
+			function( data ) {
+				$( document ).find( $container ).find( $content ).html(data);
+				$( document ).find( '#product-quick-view .cart' ).triggerQuantityButtons();
+				$( document ).find( '#product-quick-view .variations_form' ).wc_variation_form();
+				$( document ).find( '#product-quick-view .variations_form' ).trigger( 'check_variations' );
+				$( so_unwind_data.chevron_down ).insertAfter( '#product-quick-view .variations_form select' );
+			}
+		);
+
+		$( document ).ajaxComplete( function () {
+			if ( $.isFunction( $.fn.flexslider ) ) {
+				$( '.product-images-slider' ).flexslider( {
+					animation: "slide",
+					animationLoop: true,
+					slideshow: false,
+					controlNav: true,
+					directionNav: true,
+				} );
+			}
+		} );
+
+		if ( $( document ).find( $container ).is( ':hidden' ) ) {
+			$( document ).find( $container ).find( $content ).empty();
+		}
+
+		$( document ).find( $container ).fadeIn( 300 );
+
+		// Disable scrolling when quick view is open.
+		$( 'body' ).css( 'margin-right', ( window.innerWidth - $( 'body' ).width() ) + 'px' );
+		$( 'body' ).css( 'overflow', 'hidden' );
+
+		$( window ).mouseup( function( e ) {
+			var container = $( $content );
+			if ( ( ! container.is( e.target ) && container.has( e.target ).length === 0 ) || $( '.quickview-close-icon' ).is( e.target ) ) {
+				$( $container ).fadeOut( 300 );
+				// Enable scrolling.
+				$( 'body' ).css( 'overflow', '' );
+				$( 'body' ).css( 'margin-right', '' );
+			}
+		} );
+
+		$( document ).keyup( function( e ) {
+			var container = $($content);
+			if ( e.keyCode == 27 ) { // Escape key maps to keycode `27`.
+				$( $container ).fadeOut( 300 );
+				// Enable scrolling.
+				$( 'body' ).css( 'overflow', '' );
+				$( 'body' ).css( 'margin-right', '' );
+			}
+		} );
+
+	} );	
+
 	$( so_unwind_data.chevron_down ).insertAfter( '.variations select' );
 
 } );
--- woocommerce/quick-view.php
+++ woocommerce/quick-view.php
@@ -1,16 +1,15 @@
 <?php
 
-// if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
 while ( have_posts() ) : the_post();
 
 	global $post, $product;
 
 	if ( ! function_exists( 'siteorigin_unwind_woocommerce_quick_view_class' ) ) :
 	/**
-	 * Adds the product-quick-view class to the quick view post.
+	 * Adds the product-quick-view class to the Qick View post.
 	 */
 	function siteorigin_unwind_woocommerce_quick_view_class( $classes ) {
+		$classes[] = "woocommerce";
 		$classes[] = "product-quick-view";
 		return $classes;
 	}
