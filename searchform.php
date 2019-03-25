bb07c754394dc733cd994f7a9d290d7fd5d11062
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2019-03-25 19:08:40 +1000

    Search Labels: change esc_html_e to esc_attr_e

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="search" name="s" aria-label="<?php esc_html_e( 'Search for', 'siteorigin-unwind' ); ?>" placeholder="<?php esc_attr_e( 'Search', 'siteorigin-unwind') ?>" value="<?php echo get_search_query() ?>" />
-	<button type="submit" aria-label="<?php esc_html_e( 'Search', 'siteorigin-corp' ); ?>">
+	<input type="search" name="s" aria-label="<?php esc_attr_e( 'Search for', 'siteorigin-unwind' ); ?>" placeholder="<?php esc_attr_e( 'Search', 'siteorigin-unwind') ?>" value="<?php echo get_search_query() ?>" />
+	<button type="submit" aria-label="<?php esc_attr_e( 'Search', 'siteorigin-corp' ); ?>">
 		<?php siteorigin_unwind_display_icon( 'icons_search' ); ?>
 	</button>
 </form>
