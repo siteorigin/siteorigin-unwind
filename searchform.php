3855954a61c0a2bf8b47d24e655c62f8f15af941
Andrew Misplon <amisplon@gmail.com>
2019-03-26 11:48:19 +0200

    Corrected text domains

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<input type="search" name="s" aria-label="<?php esc_attr_e( 'Search for', 'siteorigin-unwind' ); ?>" placeholder="<?php esc_attr_e( 'Search', 'siteorigin-unwind') ?>" value="<?php echo get_search_query() ?>" />
-	<button type="submit" aria-label="<?php esc_attr_e( 'Search', 'siteorigin-corp' ); ?>">
+	<button type="submit" aria-label="<?php esc_attr_e( 'Search', 'siteorigin-unwind' ); ?>">
 		<?php siteorigin_unwind_display_icon( 'icons_search' ); ?>
 	</button>
 </form>
