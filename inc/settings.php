14981f66e88f4f3da52477b88bbeabc8cf1bfaa8
Andrew Misplon <amisplon@gmail.com>
2020-02-01 11:25:50 +0200

    Products per row label update

--- inc/settings.php
+++ inc/settings.php
@@ -356,7 +356,7 @@ function siteorigin_unwind_woocommerce_settings( $settings ) {
 				),
 				'archive_columns' => array(
 					'type' => 'range',
-					'label' => esc_html__( 'Number of Products per Row', 'siteorigin-unwind' ),
+					'label' => esc_html__( 'Products per Row', 'siteorigin-unwind' ),
 					'description' => esc_html__( 'Set the number of products per row on shop archive pages.', 'siteorigin-unwind' ),
 					'min' => 2,
 					'max' => 5,
